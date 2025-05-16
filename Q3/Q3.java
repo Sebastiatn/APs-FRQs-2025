030d1eb6fb2b823bd7f7cf0f939586c5b5712bcb
Sebastiatn <sebastiant87@nycstudents.net>
2025-05-16 09:32:50 -0400

    Update Q3.java

--- Q3/Q3.java
+++ Q3/Q3.java
@@ -1,1 +1,11 @@
-
+class Round{
+    private String name;
+    private int rank;
+    
+    public Round(String[] names){
+        int rank = 1;
+        for(int i:name){
+            competitorList.add(new competitor(i,rank));
+            rank++;
+        }
+    }
