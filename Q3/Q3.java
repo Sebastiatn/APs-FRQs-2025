800b4095ffd5c999bc50b18ecd7f183cdf6b90ba
Sebastiatn <sebastiant87@nycstudents.net>
2025-05-19 23:58:19 -0400

    Update Q3.java

--- Q3/Q3.java
+++ Q3/Q3.java
@@ -9,3 +9,16 @@ class Round{
             rank++;
         }
     }
+    //A
+    public Array<Match> buildMatch(){
+        ArrayList<Match> creationMatchs = new ArrayList<Match>();
+        int start = 0;
+        if(competitorList.size() % 2 == 1){
+            start = 1;
+        }
+        for(int i = 0; i <(size - start)/2; i++){
+            creationMatchs.add(competitorList.get(start + i), competitorList.get());
+        }
+    }
+    //B
+}
